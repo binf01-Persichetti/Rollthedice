dfdb93a9f679c52110a85dd848dab29d10a68ea3
binf01-Persichetti <lorypersichetti06@gmail.com>
2025-04-29 21:34:42 +0200

    aggiunto array associativo per visualizzare a schermo il risultato del lancio del dado

--- app/src/main/java/com/example/lanciodadipersichetti/MainActivityDue.kt
+++ app/src/main/java/com/example/lanciodadipersichetti/MainActivityDue.kt
@@ -1,6 +1,9 @@
 package com.example.lanciodadipersichetti
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +15,23 @@ class MainActivityDue : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_due)
 
+        val mioRandom = intent.getIntExtra("NUMERO", -1)
+        val continua = findViewById<Button>(R.id.continua)
+
+        continua.setOnClickListener {
+            val mioToast = Toast.makeText(this, "Risultato:", Toast.LENGTH_LONG)
+            mioToast.show()
+        }
+
+        val dado = findViewById<ImageView>(R.id.dado)
+        val arrayDadi = when(mioRandom){
+            1 -> R.drawable.dice_face_1
+            2 -> R.drawable.dice_face_2
+            3 -> R.drawable.dice_face_3
+            4 -> R.drawable.dice_face_4
+            5 -> R.drawable.dice_face_5
+            else -> {R.drawable.dice_face_6}
+        }
+        dado.setImageResource(arrayDadi as Int)
     }
 }
