ecd73ed5ec32ebe14293b92fd6b84e9047e35f55
binf01-Persichetti <lorypersichetti06@gmail.com>
2025-04-29 21:27:47 +0200

    aggiunto nel primo main una funziona random per simulare il lancio del dado ed una per richiamare la seconda schermata

--- app/src/main/java/com/example/lanciodadipersichetti/MainActivity.kt
+++ app/src/main/java/com/example/lanciodadipersichetti/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.lanciodadipersichetti
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val dadi = findViewById<ImageView>(R.id.dadi)
+        val titolo = findViewById<TextView>(R.id.titolo)
+        val lancia = findViewById<Button>(R.id.lancia)
+
+        lancia.setOnClickListener {
+            val mioToast = Toast.makeText(this, "Dado lanciato", Toast.LENGTH_LONG)
+            mioToast.show()
+
+            val mioRandom = numRandom()
+            lanciaIntentDue(mioRandom)
         }
+    }
+
+    private fun lanciaIntentDue(mioRandom: Int){ //Richiama il secondo activity (Lancia) passando il numero random
+        val mioIntent = Intent(this, MainActivityDue::class.java)
+        mioIntent.putExtra("NUMERO", mioRandom)
+        startActivity(mioIntent)
+    }
+
+    private fun numRandom(): Int{
+        return (1..6).random()
     }
 }
