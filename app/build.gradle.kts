9b1fdaaf041b5fd436ddd2f64863d99a8ae825f8
LorenzoPersi <lorypersichetti06@gmail.com>
2025-04-29 21:05:32 +0200

    importate le immagini dei dadi e create tre activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.lanciodadipersichetti"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lanciodadipersichetti"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
